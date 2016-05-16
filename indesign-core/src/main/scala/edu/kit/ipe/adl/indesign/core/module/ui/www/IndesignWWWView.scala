5af1d118cdbe2b3c9c5a375b89ee498c17c971fe
richnou <leys.richard@gmail.com>
2016-05-16 10:42:03 +0200

    Improved WWW UI to add a default custom view

--- indesign-core/src/main/scala/edu/kit/ipe/adl/indesign/core/module/ui/www/IndesignWWWView.scala
+++ indesign-core/src/main/scala/edu/kit/ipe/adl/indesign/core/module/ui/www/IndesignWWWView.scala
@@ -38,7 +38,7 @@ class IndesignWWWView extends LocalWebHTMLVIew with DefaultLocalWebHTMLBuilder {
         script(new URI(s"/resources/indesign.js")) {
 
         }
-        
+
         // Modules
         //----------------
         script(new URI(s"/resources/modules/heart/heart.js")) {
@@ -79,11 +79,28 @@ class IndesignWWWView extends LocalWebHTMLVIew with DefaultLocalWebHTMLBuilder {
               "item" :: a("#") {
                 textContent("InDesign")
               }
-              "active item" :: a("#") {
+              "item" :: a("#") {
                 textContent("Control")
-                +@("reRender" -> "true") 
+                reRender
                 onClick {
-                  detachView("page")
+
+                  placeView(new ControlView , "page", viewready = false)
+
+                }
+              }
+              "active item" :: a("#") {
+                textContent("Home")
+                +@("reRender" -> "true")
+                onClick {
+
+                  IndesignWWWView.defaultView match {
+                    case Some(v) =>
+                      placeView(v, "page", viewready = false)
+
+                    case None =>
+                      detachView("page")
+                  }
+
                 }
               }
               "item" :: div {
@@ -94,15 +111,15 @@ class IndesignWWWView extends LocalWebHTMLVIew with DefaultLocalWebHTMLBuilder {
                     WWWViewHarvester.onResources[IndesignUIView] {
                       moduleView =>
                         "item" :: a("#") {
-                          
+
                           //var ready = (moduleView.contentClosure==null || moduleView.proxy.isDefined)
-                          var ready =  moduleView.isProxy
-                          textContent(moduleView.name+s"(${ready})")
+                          var ready = moduleView.isProxy
+                          textContent(moduleView.name + s"(${ready})")
                           +@("reRender" -> "true")
-                          
+
                           onClick {
                             //placeView(moduleView.getClass, "page")
-                            placeView(moduleView,"page",viewready=ready)
+                            placeView(moduleView, "page", viewready = ready)
                           }
                         }
                     }
@@ -114,219 +131,38 @@ class IndesignWWWView extends LocalWebHTMLVIew with DefaultLocalWebHTMLBuilder {
             }
           }
 
+          // Main Page
+          //--------------
           viewPlaceHolder("page", "twelve wide column") {
 
-            h1("InDesign UI ") {
-
+            p {
+              textContent("No Placed View 2")
             }
-
-            /*input {
-              +@("type" -> "button")
-              +@("value" -> s"Click me 2 -> $valueSee")
-              textContent("Click me")
-              val b = currentNode
-              println(s"Building node: " + b.hashCode())
-              onClick {
-                println(s"Hello World 2 with value Seee: " + valueSee + ", changing node: " + b.hashCode())
-                b.apply("value" -> "I was Clicked")
-                valueSee = " last click on: " + System.currentTimeMillis()
-              }
-            }
-
-            input {
-              +@("type" -> "button")
-              +@("value" -> s"Click me with automatic Rerender -> $valueSee")
-              +@("reRender" -> "true")
-              textContent("Click me")
-              onClick {
-                println(s"Hello World 2 with value Seee: " + valueSee)
-                currentNode.apply("value" -> "I was Clicked")
-                valueSee = " last click on: " + System.currentTimeMillis()
-              }
-            }
-*/
-            // Brain
-            //-------------------
-            $(<h2 class="ui header">
-                <i class="settings icon"></i>
-                <div class="content">
-                  Brain
-                  <div class="sub header">Brain Region/Modules</div>
-                </div>
-              </h2>)
-
-            //-- Regions Table 
-            "ui celled sortable table" :: table {
-              thead {
-                tr {
-                  "sorted descending" :: th("Region") {
-
-                  }
-
-                  th("State") {
-
-                  }
-                  th("Error") {
-
-                  }
-                }
-              }
-              tbody {
-
-                Brain.regions.foreach {
-                  r =>
-                    tr {
-                      td(r.name) {
-
-                      }
-                      r.currentState match {
-                        case None =>
-                          "negative" :: td("Not in Lifecylce") {
-
-                          }
-                        case Some(s) =>
-                          td("") {
-                            "icon checkmark" :: i {
-
-                            }
-                            span(textContent(s))
-
-                          }
-                      }
-                      "positive" :: td("") {
-                        "icon checkmark" :: i {
-
-                        }
-                        span(textContent("None"))
-                      }
-                    }
-                }
-
-              }
-            }
-
-            // Harvest
-            //------------------
-            $(<h2 class="ui header">
-                <i class="settings icon"></i>
-                <div class="content">
-                  Harvest
-                  <div class="sub header">Harvesters and Found Resources</div>
-                </div>
-              </h2>)
-
-            //-- harvest run
-            div {
-              "ui icon button" :: button("Run Harvest") {
-                "icon settings" :: i {
-
-                }
-                reRender
-                onClick {
-                  Harvest.run
-                }
-              }
-
-            }
-
-            //-- Harvest Table 
-            "ui celled sortable table" :: table {
-              id("harvest-table")
-              thead {
-                tr {
-                  "sorted descending" :: th("Harvester") {
-
-                  }
-
-                  th("Resources Count") {
-
-                  }
-                  th("Info") {
-
-                  }
-                  th("Last Run") {
-
-                  }
-                  th("Error") {
-
-                  }
-                }
-              }
-              tbody {
-
-                def harvesterLine(hv: Harvester): Unit = {
-                  //-- Current
-                  tr {
-
-                    // Id
-                    +@("data-tt-id" -> hv.hierarchyName())
-
-                    // Parent
-                    +@("data-tt-parent-id" -> hv.hierarchyName(withoutSelf = true))
-
-                    td(hv.getClass.getSimpleName) {
-
-                    }
-
-                    td(hv.getResources.size.toString()) {
-
-                    }
-
-                    td("") {
-                      
-                    }
-                    
-                    td(hv.lastRun.toString) {
-
-                    }
-
-                    //-- Error 
-                    hv.getLastError match {
-                      case Some(e) =>
-                        "negative" :: td("") {
-                          "icon close" :: i {
-
-                          }
-                          span(textContent(e.getLocalizedMessage))
-                        }
-                      case None =>
-                        "positive" :: td("") {
-                          "icon checkmark" :: i {
-
-                          }
-                          span(textContent("None"))
-                        }
-                    }
-
-                  }
-
-                  //-- Children
-                  hv.childHarvesters.foreach {
-                    child: Harvester => harvesterLine(child)
-                  }
-                }
-                Harvest.harvesters.foreach {
-                  r =>
-                    harvesterLine(r)
-
-                }
-
-              }
-            } // EOF harvest table
-
-            script("""
-
-$(function() {
-	$("#harvest-table").treetable();
-});
-//$("#harvest-table").treetable();
-""")
 
           }
-          // EOF Page Column
+
+          IndesignWWWView.defaultView match {
+            case Some(v) =>
+              // println(s"********** Placed ***********")
+              viewPlaces.get("page") match {
+                case Some((container, null)) =>
+                  placeView(v, "page", viewready = false)
+                case _ =>
+              }
+
+            case None =>
+          }
+
+          // EOF Main Page
         }
 
       }
     }
   }
+}
+
+object IndesignWWWView {
+
+  var defaultView: Option[IndesignUIView] = None
+
 }
