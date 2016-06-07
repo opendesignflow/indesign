834fec848431be3735506e43cb9172ba52cf6eb1
richnou <leys.richard@gmail.com>
2016-06-07 15:39:53 +0200

    Added Indesign Webdraw helper module to handle draw area and dimensioning

---
+++ indesign-webdraw/src/main/scala/edu/kit/ipe/adl/indesign/core/module/webdraw/PixiWebBuilder.scala
@@ -0,0 +1,25 @@
+package edu.kit.ipe.adl.indesign.core.module.webdraw
+
+import java.net.URI
+import com.idyria.osi.vui.html.HTMLNode
+import org.w3c.dom.html.HTMLElement
+
+trait PixiWebBuilder extends WebdrawViewBuilder {
+  
+ override def externalAdd(targetNode: HTMLNode[HTMLElement, Any]): Unit = {
+    
+     super.externalAdd(targetNode)
+    switchToNode(targetNode, {
+
+      script(new URI(createSpecialPath("resources", "module/webdraw/pixi-v3/pixi.min.js"))) {
+        
+      }
+
+     
+      
+    })
+   
+
+  }
+  
+}
