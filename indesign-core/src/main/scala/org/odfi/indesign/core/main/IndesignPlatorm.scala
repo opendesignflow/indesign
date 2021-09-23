c7b96290c94b4cd23fcd3b3f547f854f9e25c489
richnou <leys.richard@gmail.com>
2021-09-23 20:50:14 +0200

    Removed IndesignPlatofrm main behavior, outdated and causing issues on projects using the class to quickly setup modules

--- indesign-core/src/main/scala/org/odfi/indesign/core/main/IndesignPlatorm.scala
+++ indesign-core/src/main/scala/org/odfi/indesign/core/main/IndesignPlatorm.scala
@@ -16,7 +16,7 @@ import org.odfi.indesign.core.brain.ExternalBrainRegion
 import org.odfi.indesign.core.heart.Heart
 import org.odfi.indesign.core.module.HarvesterModule
 
-object IndesignPlatorm extends App {
+object IndesignPlatorm  {
 
   // Utilities to help build simpler and faster mains
   //------------------
@@ -63,72 +63,5 @@ object IndesignPlatorm extends App {
     r
   }
 
-  // Indesign Standalone Main
-  //----------------------
-
-  println("Welcome to the Indesign Platform")
-
-  TLog.setLevel(classOf[Brain], TLog.Level.FULL)
-
-  //TLog.setLevel(classOf[LFCDefinition], TLog.Level.FULL)
-
-  // Brain / Config Setup
-  //-------------------
-
-  var wsName = args.indexOf("--workspace") match {
-    case -1 => new File("workspace-main").getCanonicalFile
-    case i =>
-      var targetFile = new File("workspace-" + args(i + 1)).getCanonicalFile
-      targetFile
-    /*targetFile match {
-        case f if (!f.exists || !f.isDirectory()) => sys.error("Content Folder Folder must exists and be directory")
-        case f => f
-      }*/
-
-  }
-  wsName.mkdirs
-
-  Config.setImplementation(new OOXOOFSConfigImplementation(new File(wsName, "indesign-config")))
-  Brain.deliverDirect(Harvest)
-  Brain.deliverDirect(Config)
-  Harvest.addHarvester(Brain)
-
-  //-- Add one Harvester
-  Harvest.run
-
-  Brain.moveToStart
-
-  println("============= Builder check =============")
-  ExternalBrainRegion.builders.foreach {
-    b =>
-      println(s"Available Builder: " + b + " -> " + b.getClass.getClassLoader())
-  }
-
-  /* sys.exit
-  Brain += (Config, Harvest)*/
-
-  // Init
-  //---------------
-  println(s"Start init ")
-  //Brain.init
-  println(s"Done init ")
-
-  // Run One Harvest Phase
-  //-----------
-  Harvest.printHarvesters
-
-  //Console.readLine()
-
-  Harvest.run
-  Harvest.run
-
-  println("============= Builder check =============")
-  ExternalBrainRegion.builders.foreach {
-    b =>
-      println(s"Available Builder: " + b + " -> " + b.getClass.getClassLoader())
-  }
-
-  /*Harvest.run
-  Harvest.printHarvesters*/
 
 }
